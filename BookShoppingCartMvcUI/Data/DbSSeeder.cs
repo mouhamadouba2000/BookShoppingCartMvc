9ab9fed8e4f1eb2eedb107e601d55ebb7eaef7b8
mba <mba@DESKTOP-MV9V6GP>
2024-07-11 16:45:20 +0200

    Ajoutez des fichiers projet.

---
+++ BookShoppingCartMvcUI/Data/DbSSeeder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookShoppingCartMvcUI.Data
+{
+    public class DbSSeeder
+    {
+        public static void SeedDefaultData(IServiceProvider service)
+        {
+            var userManager = service.GetService<UserManager<IdentityUser>>();
+            var roleManager = service.GetService<RoleManager<IdentityRole>>();
+            //adding some role to db
+        }
+    }
+}
